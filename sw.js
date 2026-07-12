7ab05397e66a4b3d6d925703067ce29cce94af61
nguyentai219 <nguyentai219@gmail.com>
2026-07-12 21:12:32 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mu-cao-su-v11.3';
+const CACHE = 'mu-cao-su-v11.4';
 const FILES = ['/Mucaosu/','/Mucaosu/index.html','/Mucaosu/manifest.json','/Mucaosu/icon-192.png','/Mucaosu/icon-512.png','/Mucaosu/apple-touch-icon.png','/Mucaosu/logo.png'];
 
 self.addEventListener('install', e => {
